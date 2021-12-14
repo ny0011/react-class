d6afda887720c94c34bc39fe213e0f078dd5fce2
ny0011 <nayun011@gmail.com>
2021-12-14 11:43:58 +0900

    animation을 만들 때 keyframes를 사용하자

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Input = styled.input.attrs({ required: true, minLength: 10 })`
+const rotationAnimation = keyframes`
+  0%{
+    transform: rotate(0deg);
+    border-radius: 0px
+  }
+  50% {
+    transform: rotate(360deg);
+    border-radius: 100px
+  }
+  100%{
+    transform: rotate(0deg);
+    border-radius: 0px
+  }
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
   background-color: tomato;
+  animation: ${rotationAnimation} 1s linear infinite;
 `;
 
 function App() {
   return (
-    <Father as="header">
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box />
+    </Wrapper>
   );
 }
 

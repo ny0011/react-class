4c004fc6caffa3573add95b56de30af65f1ea8d5
ny0011 <nayun011@gmail.com>
2021-12-14 11:32:58 +0900

    as 사용하기 - css는 같고 tag만 바꾸고 싶을 때

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import styled from "styled-components";
 const Father = styled.div`
   display: flex;
 `;
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
 
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+    <Father as="header">
+      <Btn>Log In </Btn>
+      <Btn as="a" href="/">
+        Log In
+      </Btn>
     </Father>
   );
 }

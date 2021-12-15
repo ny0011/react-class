23608e65c57853ead46a352f10f1f6cfe57d94c8
ny0011 <nayun011@gmail.com>
2021-12-15 15:06:31 +0900

    form
    event : any type이라서 typescript는 특정 type을 지정해줘야 에러를 내지 않음
    -> 구글링으로 찾기...ㅜ
    -> event type 체크를 해주기 때문에 더 안전하게 개발할 수 있음

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+
+  // Input element가 이 이벤트를 발생시켰다
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    // react typescript 쓰는 사람은 currentTarget을 선택함.
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
+
   return (
     <div>
-      <Circle bgColor="teal" borderColor="yellow" />
-      <Circle bgColor="tomato" text="aa" />
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="username"
+          value={value}
+          onChange={onChange}
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }

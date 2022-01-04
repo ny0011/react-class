44bf783c11542a76cf9982720bf0d12756fe59cf
ny0011 <nayun011@gmail.com>
2022-01-04 11:17:26 +0900

    Crypto Tracker - home #1
    - createGlobalStyle
    - update coins style

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
+  bgColor: "#222f3e",
+  textColor: "#c8d6e5",
+  accentColor: "#1dd1a1",
 };

2c9d9423191ca2cb59099aa6cdc239d466117478
ny0011 <nayun011@gmail.com>
2022-01-12 14:10:04 +0900

    route state
    - react router에서 link를 사용할 때 object 형태로 데이터를 더 전송할 수 있다.
    - 원하는 정보는 state에 보통 담아 보내는듯

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -23,8 +23,9 @@ const Coin = styled.li`
   margin-bottom: 10px;
   border-radius: 15px;
   a {
+    display: flex;
+    align-items: center;
     transition: color 0.2s ease-in;
-    display: block;
     padding: 20px;
   }
   &:hover {
@@ -42,6 +43,12 @@ const Title = styled.h1`
 const Loader = styled.span`
   text-align: center;
   display: block;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
 `;
 
 interface CoinInterface {
@@ -80,7 +87,18 @@ function Coins() {
         <CoinsList>
           {coins.map((coin) => (
             <Coin key={coin.id}>
-              <Link to={`/:${coin.id}`}>{coin.name} &rarr;</Link>
+              <Link
+                to={{
+                  pathname: `/${coin.id}`,
+                  state: { name: coin.name },
+                }}
+              >
+                <Img
+                  src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                  alt="coin symbol"
+                />
+                {coin.name} &rarr;
+              </Link>
             </Coin>
           ))}
         </CoinsList>
